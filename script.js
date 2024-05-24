9501175c791e7c2c302d3ca1e95b1afe5ed9d57e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-24 23:13:42 +0530

    updated button to submit

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const form = document.querySelector("form");
 const resultDiv = document.querySelector(".result");
 const button = document.querySelector("button");
+const input = document.querySelector("input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  getWordsInfo(input.value);
 });
+
+const getWordsInfo = (word) => {
+  alert("Word" + word);
+};
