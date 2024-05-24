6e0577b74ac00316fb179fcdc86f74f8c759c24a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-24 23:50:27 +0530

    added try catch

--- script.js
+++ script.js
@@ -9,10 +9,11 @@ form.addEventListener("submit", (e) => {
 });
 
 const getWordsInfo = async (word) => {
-  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-  const response = await fetch(url);
-  const data = await response.json();
-  resultDiv.innerHTML = `
+  try {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    resultDiv.innerHTML = `
   <h2><strong>WORD: </strong>${data[0].word}</h2>
   <p>${data[0].meanings[0].partOfSpeech}</p>
   <strong>Meaning: </strong> <p>${
@@ -27,16 +28,19 @@ const getWordsInfo = async (word) => {
   }</p>
   <p><strong>Antonyms: </strong></p>
   `;
-  if (data[0].meanings[0].definitions[0].antonyms.length === 0) {
-    resultDiv.innerHTML += `<span>Not Found</span>`;
-  } else {
-    for (
-      let i = 0;
-      i < data[0].meanings[0].definitions[0].antonyms.length;
-      i++
-    ) {
-      resultDiv.innerHTML += `<li>${data[0].meanings[0].definitions[0].antonyms[i]}</li>`;
+    if (data[0].meanings[0].definitions[0].antonyms.length === 0) {
+      resultDiv.innerHTML += `<span>Not Found</span>`;
+    } else {
+      for (
+        let i = 0;
+        i < data[0].meanings[0].definitions[0].antonyms.length;
+        i++
+      ) {
+        resultDiv.innerHTML += `<li>${data[0].meanings[0].definitions[0].antonyms[i]}</li>`;
+      }
     }
+    resultDiv.innerHTML += `<a href = ${data[0].sourceUrls} target = "_blank">Read More</a>`;
+  } catch (error) {
+    resultDiv.innerHTML = `<p>Word Not Found!</p>`;
   }
-  resultDiv.innerHTML += `<a href = ${data[0].sourceUrls} target = "_blank">Read More</a>`
 };
