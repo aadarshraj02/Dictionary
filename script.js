3c5d47695e929da6fc53b90f5a59de11fa463b69
aadarshraj02 <aadarshraazz007@gmail.com>
2024-05-24 23:03:54 +0530

    added a event listener on form and passed e to prevent default refresh on button click

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const form = document.querySelector("form");
 const resultDiv = document.querySelector(".result");
 const button = document.querySelector("button");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
